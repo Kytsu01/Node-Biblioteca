b7502a758ae3b520f1fbd3d890d953163f1d70e9
Nicolas <blackoskiller959@gmail.com>
2025-10-22 22:21:07 -0300

    feat: Comecar a fazer sistema de login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { titulo: 'Sistema de Biblioteca' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.send('Aqui vai aparecer um forum - aguarde...');
+});
+
+router.post('/login', function(req, res, next) {
+  const {usuario, senha} = req.body;
+
+  if(usuario == "nicolas@gmail.com" && senha === "123456") {
+    res.send("Logado com sucesso!");
+  } else {
+    res.render('index', {titulo : "Sistema de Biblioteca", erro : "Usuario/Senha invalidos!"});
+  }
+});
+
+router.get('/listagem', function(req, res, next) {
+  res.send('Aqui vai aparecer a lista de livros - aguarde...');
 });
 
 module.exports = router;
